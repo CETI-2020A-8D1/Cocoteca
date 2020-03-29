1db9956005496ed0f775f4d71005fd98df4cef7b
judith-ale <62565005+judith-ale@users.noreply.github.com>
2020-03-29 15:48:15 -0600

    Recibe categoría y listado de libros para carrusel

---
+++ Models/Cliente/InicioModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cocoteca.Models
+{
+    public class Inicio
+    {
+        public int idcategoria { get; set; }
+        public string nombre { get; set; }
+        public List<MtoCatLibroItem> mtoCatLibros { get; set; }
+    }
+}
