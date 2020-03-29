a09d413cef3ac29ca70c6d8d8e2034f14706565e
Angel <padilla.angel88@gmail.com>
2020-03-28 19:00:33 -0600

    CarritoCompra modelo

---
+++ Models/CarritoCompra.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cocoteca.Models
+{
+    public  class CarritoCompra
+    {
+        TraConceptoCompra conceptoCompra { get; set; }
+        MtoCatLibros libro { get; set; }
+
+        public CarritoCompra(TraConceptoCompra conceptoCompra, MtoCatLibros libro)
+        {
+            this.conceptoCompra = conceptoCompra;
+            this.libro = libro;
+        }
+    }
+}
