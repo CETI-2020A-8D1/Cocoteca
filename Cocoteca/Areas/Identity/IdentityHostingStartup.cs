3e861c696570be485f2e8df80f080415af717bf3
judith-ale <62565005+judith-ale@users.noreply.github.com>
2020-04-08 17:18:08 -0500

    Todo nuevo con identity y código cambiado

---
+++ Cocoteca/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Cocoteca.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Cocoteca.Areas.Identity.IdentityHostingStartup))]
+namespace Cocoteca.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                
+            });
+        }
+    }
+}
---
+++ Cocoteca/Helper/CocontroladorAPI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Cocoteca.Helper
+{
+    public class CocontroladorAPI
+    {
+        public static string Initial()
+        {
+            return "https://localhost:44341/";
+        }
+    }
+
+}
