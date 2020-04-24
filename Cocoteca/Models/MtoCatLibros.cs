fbc540e17a969d51d1091be6aeadffa5e3c0b27c
Ricardo <ricardoglr664@gmail.com>
2020-04-24 15:55:44 -0500

    as

--- Cocoteca/Models/MtoCatLibros.cs
+++ Cocoteca/Models/MtoCatLibros.cs
@@ -16,7 +16,7 @@ namespace Cocoteca.Models
         public string Isbn { get; set; }
         public string Titulo { get; set; }
         public string Autor { get; set; }
-        public string Sinopsis { get; set; }
+        public string Sinopsis { get; set; } 
         public bool Descontinuado { get; set; }
         public int Paginas { get; set; }
         public int Revision { get; set; }
