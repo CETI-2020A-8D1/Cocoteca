30b932d7c7f9ab55730bfa834872a43a7efd9953
Cristopher Ceja <a16300065@ceti.mx>
2020-06-03 19:40:03 -0500

    Agregado comentarios Controladores Cocoppel API

--- Controllers/TarjetaController.cs
+++ Controllers/TarjetaController.cs
@@ -11,19 +11,33 @@ using Newtonsoft.Json;
 
 namespace Cocoteca.Controllers
 {
+    /// <summary>
+    /// Clase para controlar la tarjeta de crédito en /Tarjeta/Tarjeta
+    /// </summary>
     public class TarjetaController : Controller
     {
         static readonly HttpClient client = new HttpClient();
         int precio = 200;
 
+        /// <summary>
+        /// HTTPGet Tarjeta
+        /// </summary>
+        /// <returns>La vista /Tarjeta/Tarjeta, un formulario para introducir datos de tarjeta de crédito o débito</returns>
         [HttpGet]
         public ActionResult Tarjeta()
         {
             return View();
         }
+
+        /// <summary>
+        /// HTTPPost Tarjeta
+        /// </summary>
+        /// <param name="usuario">Tarjeta devuelta por el formulario /Tarjeta/Tarjeta</param>
+        /// <returns></returns>
         [HttpPost]
         public async Task<IActionResult> Tarjeta(Tarjeta usuario)
         {
+            // Revisa si el campo del titular o el número esta vacío
             if (string.IsNullOrEmpty(usuario.Titular) ||
             string.IsNullOrEmpty(usuario.Numero))
             {
@@ -35,8 +49,10 @@ namespace Cocoteca.Controllers
                 string tipo = usuario.Tipo;
                 tipo = usuario.Tipo;
 
+                // La transacción toma en cuenta la tarjeta del usuario, la cuenta de depósito de Cocoteca (índice 1 en la base de datos), y el precio
                 Transaccion transaccion = new Transaccion(usuario, 1, precio);
 
+                // Serializar en Json el objeto transacción para ser pasado por HTTPPost
                 var content = JsonConvert.SerializeObject(transaccion);
 
                 var buffer = System.Text.Encoding.UTF8.GetBytes(content);
@@ -47,6 +63,7 @@ namespace Cocoteca.Controllers
 
                 string action;
 
+                // Revisar si es tarjeta de crédito o débito para llamar el controlador indicado
                 if (tipo.Equals("Credito"))
                 {
                     action = "api/Pagartc";
@@ -57,6 +74,7 @@ namespace Cocoteca.Controllers
                 }
                 Console.WriteLine(CocoppelAPI.Initial() + action);
 
+                // LLamar el controlador y esperar estado 200 OK
                 var response = await client.PostAsync(CocoppelAPI.Initial() + action, byteContent);
                 response.EnsureSuccessStatusCode();
 
@@ -64,6 +82,7 @@ namespace Cocoteca.Controllers
             }
             catch (HttpRequestException e)
             {
+                // Si hubo un error al llamar el controlador, retorna la misma vista con los datos del cliente (para que no vuelva a introducir datos) pero con un mensaje de error 
                 ViewBag.Error = "No se encontró la tarjeta";
                 return View(usuario);
             }
--- Models/Transaccion.cs
+++ Models/Transaccion.cs
@@ -5,13 +5,39 @@ using System.Threading.Tasks;
 
 namespace Cocoteca.Models
 {
+    /// <summary>
+    /// Clase transacción
+    /// Usado para pasar datos en el cuerpo de HTTPPost cuando se manda información sobre una compra en línea
+    /// Ponerlo en el cuerpo del HTTPRequest es más seguro que exponerlo en la URI
+    /// </summary>
     public partial class Transaccion
     {
+        /// <summary>
+        /// Tarjeta
+        /// La tarjeta del cliente al que se le hará cargo
+        /// </summary>
         public Tarjeta Tarjeta { get; set; }
+
+        /// <summary>
+        /// Numero de cuenta
+        /// Cuenta a la que se deposita
+        /// </summary>
         public int NumeroCuenta { get; set; }
+
+        /// <summary>
+        /// Precio
+        /// La cantidad o monto que se le cargara
+        /// </summary>
         public int Precio { get; set; }
 
         public Transaccion() { }
+
+        /// <summary>
+        /// Transaccion
+        /// </summary>
+        /// <param name="tarjeta">Inicializar tarjeta</param>
+        /// <param name="numerodeCuenta">Inicializar numerodeCuenta</param>
+        /// <param name="precio">Inicializar precio</param>
         public Transaccion(Tarjeta tarjeta, int numerodeCuenta, int precio) {
             Tarjeta = tarjeta;
             NumeroCuenta = numerodeCuenta;
